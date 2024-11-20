4219d02559c8396c5a1b55a5bc39afb4c0525bd5
Thaufiq Siraj <thaufiqsirajc@gmail.com>
2024-11-20 18:33:06 +0530

    Stash changes before rebasing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/react-tictac/", // Ensure this matches your repo name
+  build: {
+    assetsDir: "assets", // Ensure assets go to 'assets/'
+  },
+});
