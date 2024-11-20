d1f8761fe6b60b497ac795a3f50e87c950c6e9e2
Thaufiq Siraj <thaufiqsirajc@gmail.com>
2024-11-20 18:39:52 +0530

    Build site for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/react-tictac/", // Ensure this matches your repo name
-  build: {
-    assetsDir: "assets", // Ensure assets go to 'assets/'
-  },
-});
+  plugins: [react()],
+  base: '/react-tictac/',  // Add this line
+})
